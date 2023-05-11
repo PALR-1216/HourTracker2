86b58438fe9e5aa53a20becb2b401e5b88de3ea6
PALR-1216 <palr12162000@gmail.com>
2023-05-11 11:18:08 -0400

    dfg

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import Jsontoken from 'jsonwebtoken';
 import cookie from 'cookie-session';
 import { nanoid } from 'nanoid'
 import multer from 'multer'
-import sharp from 'sharp'
+// import sharp from 'sharp'
 //https://stackoverflow.com/questions/18441698/getting-time-difference-between-two-times-in-javascript
 
 
