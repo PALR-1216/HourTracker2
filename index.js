90c4c91498ae986a0de387558b64832c160657f4
Pedro Lorenzo Rosario <63958751+PALR-1216@users.noreply.github.com>
2023-08-16 20:15:46 +0000

    dfdf

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ app.use(session({
 }))
 
 app.get('/checkUserPayOut', async (req, res) => {
-  await getUserInfo(req.cookies.user_id);
+  await getUserInfo(req.cookies.user_id );
   // return res.send('ok')
   //it works need to make the my checkMaker bot inside here
 })  
